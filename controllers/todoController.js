1efa807b85b40b39576edfb931ed50f2169a6a68
siddhesh-athalekar <siddhesh85@gmail.com>
2019-07-30 12:14:11 +0530

    Setup dummy data for GET request

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,6 +1,18 @@
+const data = [
+    {
+        item: 'get milk'
+    },
+    {
+        item: 'walk dog'
+    },
+    {
+        item: 'code your app'
+    }
+]
+
 module.exports = function(app){
     app.get('/todo', function(req, res){
-        res.render('todo')
+        res.render('todo', {todos: data})
     })
 
     app.post('/todo', function(req, res){
